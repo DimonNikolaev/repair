e3074d21325ace8ab9f988ab13b8dfbd170b1785
Dima Nikolaev <dmitriy.nikolaev1337@gmail.com>
2019-08-05 12:52:36 +0500

    Замен обычный скрипт на скрипт jquery

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
-var button = document.querySelector('#button');
-var modal = document.querySelector('#modal');
-var close = document.querySelector('#close');
+// var button = document.querySelector('#button');
+// var modal = document.querySelector('#modal');
+// var close = document.querySelector('#close');
 
 
 
-button.addEventListener('click', function () {
-  modal.classList.add('modal_active');
-});
+// button.addEventListener('click', function () {
+//   modal.classList.add('modal_active');
+// });
 
-close.addEventListener('click', function () {
-  modal.classList.remove('modal_active');
-});
+// close.addEventListener('click', function () {
+//   modal.classList.remove('modal_active');
+// });
 
-function closeModal() {
-  modal.classList.remove('modal_active');
-}
+// function closeModal() {
+//   modal.classList.remove('modal_active');
+// }
 
-setTimeout(closeModal, 5000);
+// setTimeout(closeModal, 5000);
