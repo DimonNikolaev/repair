6ef279669073d2263bb98857e67c0220d8e1fcf1
Dima Nikolaev <dmitriy.nikolaev1337@gmail.com>
2019-08-20 00:02:24 +0500

    Добавил всплывающее окно при успешной отправке формы

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-$(document).ready(function () {
-  var modal = $('#modal');
-  var close = $('#close');
-
   $('#offer-form').on('submit', function (event) {
     event.preventDefault();
     $.ajax({
@@ -17,8 +13,7 @@ $(document).ready(function () {
     })
   })
   $('#offer-form').on('submit', function () {
-    modal.addClass('modal_active')
-    $('.modal-dialog__title').text('Спасибо за заявку, скоро мы вам перезвоним.');
-    $('.offer__input').val("");
+    // modal.addClass('modal_active')
+    // $('.modal-dialog__title').text('Спасибо за заявку, скоро мы вам перезвоним.');
+    // $('.offer__input').val("");
   })
-})
